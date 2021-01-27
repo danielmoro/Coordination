abede9e1751fdc318fa567387a6816befeaddcd3
danielmoro <daniel@ezderm.com>
2021-01-27 18:25:07 +0100

    added example code in documentation adn removed demo classes

--- Sources/Coordination/Coordinator.swift
+++ Sources/Coordination/Coordinator.swift
@@ -4,6 +4,48 @@
 //
 //  Created by Daniel Moro on 18.1.21..
 //
+/**
+  - Note: Removed `var router: Router` in protocol from original pattern. Only benefit is automatic call of dismiss method,
+    while keeping it in here preventing me from using generic routers with different techologies like `UINavigationController` and `SwiftUI`
+ ### UIKit Usage Example
+ ````
+ class DemoUIKitCoordinator: Coordinator {
+         init(navigationController: UINavigationController) {
+             router = UINavigationRouter(navigationController: navigationController)
+         }
+
+         var children: [Coordinator] = []
+
+         func present(animated: Bool, onDismissed: (() -> Void)?) {
+             let mainVC = UIViewController()
+             router.present(mainVC, animated: animated, onDismissed: nil)
+         }
+
+         func dismiss(animated: Bool) {
+             router.dismiss(animated: animated)
+         }
+
+         private var router: UIViewControllerRouter
+     }
+ ````
+ ### SwiftUI Usage Example
+ ````
+ class DemoSwiftUICoordinator: Coordinator {
+     var children: [Coordinator] = []
+
+     func present(animated: Bool, onDismissed: (() -> Void)?) {
+         let mainView = EmptyView()
+         router.setRoot(mainView)
+     }
+
+     func dismiss(animated: Bool) {
+         router.dismiss()
+     }
+
+     var router: Router = Router(isPresented: .constant(false))
+ }
+ ````
+ */
 
 public protocol Coordinator: AnyObject {
     var children: [Coordinator] { get set }
